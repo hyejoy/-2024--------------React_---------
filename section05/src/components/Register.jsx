7728adbdb7dc65069e0c190718e52f76aa89bf31
DevCHJ <devchoi97@naver.com>
2024-11-23 21:29:15 +0900

    Reference 객체 리랜더링여부 확인

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-// 
+import { useState, useRef } from "react";
+//
 /**
  * 간단한 회원가인폼
  * 1. 이름
@@ -9,59 +9,66 @@ import {useState} from "react";
  */
 
 const Register = () => {
- 
-    // 여러개 비슷한 state가 있을때는 하나의 객체로 묶어 하나의 state로 
-    // 통합관리 하면 편하다.
-    const [input, setInput] = useState({
-        name :"",
-        birth: "",
-        country : "",
-        bio : ""
-     }
-    )
+  // 여러개 비슷한 state가 있을때는 하나의 객체로 묶어 하나의 state로
+  // 통합관리 하면 편하다.
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
-    // 비슷하게 생긴 이벤트 핸들러들을 통합 이벤트 핸들러로 묶을 수 있다.
-    const onChange = (e) => {
-        console.log(e.target.name , e.target.value);
-        setInput({
-            ...input,
-            //✨프로퍼티 키를 자리에 대괄호를 쓰고 그안에 어떤 변수의 값을 쓰면
-            // 그값이 프로퍼티의 키값으로 설정된다
-            [e.target.name] : e.target.value,
-        })
-    }
+  const refObj = useRef(0);
+  console.log("Register 랜더링");
 
-    return (
-        <>
-        <div>
-            <input 
-            name="name"
-            value={input.name}
-            onChange={onChange}
-            type="text" placeholder="이름" />
-        </div>
-        <div>
-            <input name="date" type="date" onChange={onChange}/>
-            {input.date}
-        </div>
-        <div>
-            {input.country}
-            <select 
-            onChange={onChange}
-            name="country" id="">
-                <option value=""></option>
-                <option value={"kr"}>한국</option>
-                <option value={"us"}>미국</option>
-            </select>
-        </div>
-        <div>
-            {input.bio}
-            <textarea 
-            onChange={onChange}
-            name="bio" id=""></textarea>
-        </div>
-        </>
-    )
+  // 비슷하게 생긴 이벤트 핸들러들을 통합 이벤트 핸들러로 묶을 수 있다.
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      //✨프로퍼티 키를 자리에 대괄호를 쓰고 그안에 어떤 변수의 값을 쓰면
+      // 그값이 프로퍼티의 키값으로 설정된다
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          refObj.current++;
+          console.log(refObj.current);
+        }}
+      >
+        ref값 출력
+      </button>
+      <div>
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          type="text"
+          placeholder="이름"
+        />
+      </div>
+      <div>
+        <input name="date" type="date" onChange={onChange} />
+        {input.date}
+      </div>
+      <div>
+        {input.country}
+        <select onChange={onChange} name="country" id="">
+          <option value=""></option>
+          <option value={"kr"}>한국</option>
+          <option value={"us"}>미국</option>
+        </select>
+      </div>
+      <div>
+        {input.bio}
+        <textarea onChange={onChange} name="bio" id=""></textarea>
+      </div>
+    </>
+  );
 };
 
 export default Register;
