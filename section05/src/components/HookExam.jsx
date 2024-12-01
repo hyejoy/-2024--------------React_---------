c6c73c1a68a5f37ae1ff9aa7064e264c30bc72f1
DevCHJ <devchoi97@naver.com>
2024-12-01 22:10:00 +0900

    중복hook 리팩토링전

--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -4,13 +4,21 @@ import { useState } from "react";
 /**
  * 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
  * 2. 조건부로 호출될 수 없다.
+ * 3. 나만의 훅 (Cutomer hook) 직접 만들 수 있다.
  *  */
 
 const HookExam = () => {
-  if (true) {
-    const state = useState();
-  } // 조건부로 hook을 호출해서는안된다. ⚠ 오류발생
-  return <div>hookExam</div>;
+  const [input, setInput] = useState("");
+
+  const onChagne = (e) => {
+    setInput(e.targert.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={input} onChange={onChagne} />
+    </div>
+  );
 };
 
 export default HookExam;
