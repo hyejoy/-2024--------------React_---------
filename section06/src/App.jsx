f00192c22af7aa92249a7af25ddf19b039ed6e9d
hyejoy <devchoi97@gmail.com>
2024-12-12 22:35:47 +0900

    기능구현하기

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
 import Viwer from "./components/Viewr";
 import Controller from "./components/Controller";
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+  const onClickButton = (value)=>{
+    value = Number(value);
+    setCount(count + value);
+  }
+  
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viwer />
+        <Viwer count={count}/>
       </section>
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton}/>
       </section>
     </div>
   );
--- section06/src/components/Controller.jsx
+++ section06/src/components/Controller.jsx
@@ -1,10 +1,13 @@
-const Controller = () => {
-  const number = [-1, -10, -100, +100, +10, +1];
+
+const Controller = ({onClickButton}) => {
+  const number = ['-1', '-10', '-100', '+100', '+10', '+1'];
 
   return (
     <div>
       {number.map((num, idx) => (
-        <button key={idx}>{num}</button>
+        <button key={idx}
+        onClick={ () => onClickButton(num)}
+        >{num}</button>
       ))}
     </div>
   );
