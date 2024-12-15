4f3c892b090ff9adb8263ff4ecd3ada437f78357
hyejoy <devchoi97@gmail.com>
2024-12-15 22:19:15 +0900

    클린업, 정리함수 추가 정리

--- section07/src/components/Even.jsx
+++ section07/src/components/Even.jsx
@@ -5,7 +5,10 @@ const Even = () => {
     // useEffect의 콜백함수를 반환하는 함수를
     // 클린업, 정리함수라고 부르는데
     // useEffect가 끝날때 실행된다.
-    return () => {};
+    // 해당컴포넌트의 메모리를 해제하는 최적화함수를 진행할 수 있다.
+    return () => {
+      console.log("unmount");
+    };
   }, []);
 
   return <div>짝수 입니다.</div>;
