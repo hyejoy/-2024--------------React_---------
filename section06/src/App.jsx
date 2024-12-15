022edd775811dfc8d34c7a48e1368edec2ee5457
hyejoy <devchoi97@gmail.com>
2024-12-15 11:14:32 +0900

    section06_보충설명

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -4,20 +4,24 @@ import Controller from "./components/Controller";
 import { useState } from "react";
 
 function App() {
+  /** Viewr 컴포넌트와 Controller컴포넌트는 부모, 자식관게를
+   * 갖지 못하므로 값을 공유할수있는 방법이 없다.
+   * 따라서 두개의 컴포넌트의 부모인 App에서 state를 관리해야한다.
+   */
   const [count, setCount] = useState(0);
-  const onClickButton = (value)=>{
+  const onClickButton = (value) => {
     value = Number(value);
     setCount(count + value);
-  }
-  
+  };
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viwer count={count}/>
+        <Viwer count={count} />
       </section>
       <section>
-        <Controller onClickButton={onClickButton}/>
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   );
