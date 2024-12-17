e8f683a95f8e81eca1235793672ac077d20fa7ac
hyejoy <devchoi97@gmail.com>
2024-12-17 23:03:56 +0900

    TO DO LIST UI 구현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,5 +1,16 @@
+import Header from "./components/Header";
+import Editer from "./components/Editer";
+import List from "./components/List";
+import "./App.css";
+
 const App = () => {
-  return <div>TO DO LIST</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Editer />
+      <List />
+    </div>
+  );
 };
 
 export default App;
---
+++ section08/src/components/List.jsx
@@ -0,0 +1,17 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List 🌼</h4>
+      <input type="text" placeholder="검색어를 입력해주세요." />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default List;
