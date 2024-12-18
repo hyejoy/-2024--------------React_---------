b79ebf2df4f6b44b2f259a9f20f55cd29ddac5cf
hyejoy <devchoi97@gmail.com>
2024-12-18 22:40:25 +0900

    기능 구현 준비하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,31 @@
+import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Editer from "./components/Editer";
 import List from "./components/List";
-import "./App.css";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "이직 준비하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "운동하기",
+    date: new Date().getTime(),
+  },
+];
 const App = () => {
+  const [todo, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
