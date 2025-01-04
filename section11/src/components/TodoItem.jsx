1d1e6d2beedabf9d0eda8d221dd5eb46cc8836c5
hyejoy <devchoi97@gmail.com>
2025-01-04 22:47:38 +0900

    React.memo() 복습

--- section11/src/components/TodoItem.jsx
+++ section11/src/components/TodoItem.jsx
@@ -18,22 +18,9 @@ const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
   );
 };
 
+/**
+ * memo 메소드는 인수로 받은 컴포넌트를 props 가 변경되지 않았을때에는
+ * 리랜더링 하지않도록 최적화하여 반환해준다.
+ * export문에서 메모이즈된 컴포넌트를 내보내면 된다.
+ */
 export default memo(TodoItem);
-
-// 고차 컴포넌트(HOC) : 컴포넌트를 인수로 받아 해당컴포넌트의 최적화나 메모이제이션가틍ㄴ
-// 추가적인 기능이 추가된 컴포넌트를 반환하는 메모와같은 메소드
-// export default memo(TodoItem, (prevProps, nextProps) => {
-//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단 [얕은비교로 판단]
-//   // True -> Props 바뀌지 않음 => 리랜더링 X
-//   // False -> Props 바뀜 => 리랜더링 O
-
-//   // 리랜더링 O
-//   if (prevProps.id !== nextProps.id) return false;
-//   if (prevProps.isDone !== nextProps.isDone) return false;
-//   if (prevProps.content !== nextProps.content) return false;
-//   if (prevProps.date !== nextProps.date) return false;
-
-//   // 나머지는 리랜더링 X
-//   // 따라서 onCreate 함수와 onUpdate 함수가 같으면, 리랜더링하지않음
-//   return true;
-// });
