fffaf661894e3d3eeb696065269b3d8b4c6bfc21
hyejoy <devchoi97@gmail.com>
2025-01-20 22:26:24 +0900

    20250120

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   /** Query String 가져오는 방법
    * 예시 http://localhost:5173/?value=hello
    */
+
   const [params, setParmas] = useSearchParams();
   console.log(params.get('value'));
   return <div>home</div>;
