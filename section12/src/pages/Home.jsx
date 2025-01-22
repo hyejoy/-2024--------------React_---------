2a9a2d664c0126fa59084667d4d6e2019665b991
hyejoy <devchoi97@gmail.com>
2025-01-22 22:14:54 +0900

    폰트, 이미지, 레이아웃 설정하기

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import {useSearchParams} from 'react-router-dom';
 
 const Home = () => {
   /** Query String 가져오는 방법
-   * 예시 http://localhost:5173/?value=hello
+   * 예시 http://localhost:5173/?value=hello&name=hyejo
    */
-
   const [params, setParams] = useSearchParams();
+  console.log(params);
   console.log(params.get('value'));
+  console.log(params.get('name'));
+
   return <div>home</div>;
 };
 
