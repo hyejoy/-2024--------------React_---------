4dcbb250edf84852be64b94468ef23389b911ca5
hyejoy <devchoi97@gmail.com>
2025-02-06 23:23:25 +0900

    diary  페이지 개발중

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,13 +1,32 @@
 import Header from '../components/Header';
 import Button from '../components/Button';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import {getEmotionImage} from '../util/get-emotion-image.js';
+import {useContext, useEffect, useState} from 'react';
+import {DiaryStateContext} from '../App.jsx';
 
 const Diary = () => {
   /** url의 파라미터의 값을 가져오는 custom hook
    * 예시 http://localhost:5173/diary/100
    */
-  const id = useParams().id;
+  const params = useParams();
+  const nav = useNavigate();
+  const data = useContext(DiaryStateContext);
+  const [todayDiary, setTodayDiary] = useState();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      item => String(item.id) === String(params.id),
+    );
+
+    console.log('diary Object ', currentDiaryItem);
+
+    if (!currentDiaryItem) {
+      window.alert('존재하지 않는 일기입니다.');
+      nav('/', {replace: true});
+    }
+    setTodayDiary(currentDiaryItem);
+  }, [params.id]);
 
   return (
     <div>
@@ -18,7 +37,11 @@ const Diary = () => {
       />
       <h4>오늘의 일기</h4>
       <section>
-        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(todayDiary?.emotionId)} />
+      </section>
+      <h4>오늘의 일기 내용</h4>
+      <section>
+        <div>{todayDiary?.content}</div>
       </section>
     </div>
   );
