9ff2397668d9be3e67c5ce0b2e1e74e365ff4695
heyjoy <devchoi97@gmail.com>
2025-02-06 17:56:32 +0900

    diary 수정정

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,13 +1,28 @@
 import {useParams} from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import {getEmotionImage} from '../util/get-emotion-image';
 
 const Diary = () => {
   /** url의 파라미터의 값을 가져오는 custom hook
    * 예시 http://localhost:5173/diary/100
    */
   const params = useParams();
-  console.log(params);
+  const id = params.id;
 
-  return <div>{`${params.id}번 일기 입니다.`}</div>;
+  return (
+    <div>
+      <Header
+        title={'yyyy--mm--dd 기록록'}
+        leftChild={<Button text={'< 뒤로 가기'} />}
+        rightChild={<Button text={'수정하기'} />}
+      />
+      <section>
+        <h4>오늘의 감정</h4>
+        <section>{getEmotionImage(id)}</section>
+      </section>
+    </div>
+  );
 };
 
 export default Diary;
